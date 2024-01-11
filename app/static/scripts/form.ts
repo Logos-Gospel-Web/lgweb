a49691bf3eab6860189cd90322c0e636d8077301
KithLo <26156452+KithLo@users.noreply.github.com>
2024-01-11 22:23:20 +0800

    Ensure form fake input is not filled

---
+++ app/static/scripts/form.ts
@@ -0,0 +1,9 @@
+const fakeInputs = document.getElementsByClassName('contact__input--first')
+
+for (let i = 0, n = fakeInputs.length; i < n; i += 1) {
+    const fakeInput = fakeInputs[i]
+    setInterval(() => {
+        // clear fake input in case of autocomplete
+        ;(fakeInput as HTMLInputElement).value = ''
+    }, 1000)
+}
