fdca98b3efbaabb0ef71f7211bc295dd4d0f7815
KithLo <26156452+KithLo@users.noreply.github.com>
2024-04-27 20:58:28 +0800

    Remove unused alias

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,8 @@
-import path from 'path'
 import { babel } from '@rollup/plugin-babel'
-import alias from '@rollup/plugin-alias'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 
 const isProd = process.env.MODE === 'production'
-
-function resolve(...args) {
-    return path.resolve('..', '..', ...args)
-}
 
 /** @type {import('rollup').RollupOptions} */
 export default {
@@ -19,11 +13,6 @@ export default {
         nodeResolve({
             browser: true,
             extensions: ['.js', '.ts'],
-        }),
-        alias({
-            entries: [
-                { find: '@', replacement: resolve('app', 'static', 'scripts') },
-            ],
         }),
         babel({
             extensions: ['.js', '.ts'],
