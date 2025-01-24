49d756c2c75fe6108d37a3065b2979086b75365f
KithLo <26156452+KithLo@users.noreply.github.com>
2025-01-24 12:46:41 +0800

    Fix manifest

--- app/views/manifest.py
+++ app/views/manifest.py
@@ -1,16 +1,22 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from django.utils import translation
 from django.utils.translation import gettext as _
 from django.views.decorators.cache import cache_page
 
-from .common import use_etag, view_func
+from .common import get_build_version, is_valid_language, use_etag
+from ..models import to_locale
 
-@view_func
 @use_etag()
 @cache_page(None)
 def manifest(request: HttpRequest, lang) -> HttpResponse:
-    context = request.context
+    if not is_valid_language(lang):
+        return HttpResponseNotFound()
+
+    translation.activate(to_locale(lang))
+
     return render(request, 'site/manifest.json', {
-        **context,
+        'language': lang,
+        'build_version': get_build_version(),
         'base_title': _("聖道福音網"),
     }, content_type='application/json; charset=utf-8')
