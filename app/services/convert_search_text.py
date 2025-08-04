a1aca3f4ca06f4e8bfe991eff51e6439ad7fd334
KithLo <26156452+KithLo@users.noreply.github.com>
2025-08-04 13:41:05 +0800

    Fix search text handling for empty string

--- app/services/convert_search_text.py
+++ app/services/convert_search_text.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 
 def convert_search_text(title: str, html: str) -> str:
+    if not title:
+        return ''
+    if not html:
+        return title.lower()
     soup = BeautifulSoup(html, 'lxml')
     for div in soup.find_all('div', class_='box'):
         div.decompose()
